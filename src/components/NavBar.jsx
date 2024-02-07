307c632f736038c88145ebf8d133dfca9e90b246
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-02-07 12:31:11 -0300

    Update logo size and navbar toggle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,22 +25,25 @@ const NavBar = () => {
                         <img
                             className='d-inline d-sm-none' // se muestra en pantallas xs y se oculta en sm y superiores
                             src="/logo.png"
-                            width="50" // tamaño más pequeño para pantallas xs
-                            height="50"
+                            width="80" // tamaño más pequeño para pantallas xs
+                            height="80"
                             alt="Logo"
                         />
                     </NavLink>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className='navbar-dark'/>
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto align-items-center mb-3 mb-lg-0">
                     <NavLink onClick={() => setNavExpanded(false)} to="/" className={`me-auto px-3 py-2 rounded-pill text-light text-decoration-none enlace ${setActiveClass("/")}`}>Home</NavLink>
                     <NavLink onClick={() => setNavExpanded(false)} to="/nosotros" className={`me-auto px-3 py-2 rounded-pill text-light text-decoration-none enlace ${setActiveClass("/nosotros")}`}>Nosotros</NavLink>
-                    <NavLink onClick={() => setNavExpanded(false)} to="/preguntas-frecuentes" className={`me-auto px-3 py-2 rounded-pill text-light text-decoration-none enlace ${setActiveClass("/preguntas-frecuentes")}`}>Preguntas Frecuentes</NavLink>
+                    <NavLink onClick={() => setNavExpanded(false)} to="/preguntas" className={`me-auto px-3 py-2 rounded-pill text-light text-decoration-none enlace ${setActiveClass("/preguntas")}`}>Preguntas</NavLink>
                     <NavLink onClick={() => setNavExpanded(false)} to="/productos" className={`me-auto px-3 py-2 rounded-pill text-light text-decoration-none enlace ${setActiveClass("/productos")}`}>Productos</NavLink>
                     <NavLink onClick={() => setNavExpanded(false)} to="/contacto" className={`me-auto px-3 py-2 rounded-pill text-light text-decoration-none enlace ${setActiveClass("/contacto")}`}>Contacto</NavLink>
                     <NavLink onClick={() => setNavExpanded(false)} to="/" className='me-auto px-3'>
-                        <Button className='btn bg-secondary py-2 mt-2 mt-lg-0 rounded-pill shadow-lg'><i className="bi bi-cart-fill"></i></Button>
+                        <Button className='btn py-2 mt-2 mt-lg-0 rounded-pill'><i className="fs-5 bi bi-person-fill"></i></Button>
+                    </NavLink>
+                    <NavLink onClick={() => setNavExpanded(false)} to="/" className='me-auto px-3'>
+                        <Button className='btn py-2 mt-2 mt-lg-0 rounded-pill'><i className="fs-5 bi bi-cart-fill"></i></Button>
                     </NavLink>
                 </Nav>
                 </Navbar.Collapse>
