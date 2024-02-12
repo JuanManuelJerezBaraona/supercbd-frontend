28a1fd70d3aae2315ff23fb160de1575d9514744
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-02-12 15:27:44 -0300

    Refactor UserProvider in UserContext.jsx to initialize cart with data from LocalStorage

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,11 +4,11 @@ import { createContext, useState, useEffect } from "react";
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-    // // Inicializa el carrito con los datos almacenados en LocalStorage o un arreglo vacío si no hay datos.
-    // const initialCart = JSON.parse(localStorage.getItem("cart")) || [];
+    // Inicializa el carrito con los datos almacenados en LocalStorage o un arreglo vacío si no hay datos.
+    const initialCart = JSON.parse(localStorage.getItem("cart")) || [];
 
     const [allProducts, setAllProducts] = useState([]); // Aquí se guardan los productos
-    const [cart, setCart] = useState([]); // Aquí se guardan los productos que el usuario agrega al carrito
+    const [cart, setCart] = useState(initialCart); // Aquí se guardan los productos que el usuario agrega al carrito
     const [totalToPay, setTotalToPay] = useState(0); // Guarda el total a pagar $.
 
     // Guarda el carrito en LocalStorage cuando cambie.
