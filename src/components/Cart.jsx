2ffb809c2a64d15156baaa8bc08cd0c6b8e455ab
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-02-12 16:03:59 -0300

    Refactor Cart component to update quantity and display toast messages

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -22,8 +22,48 @@ const Cart = () => {
 
     // Actualizar el total a pagar cuando cambie el carrito.
     useEffect(() => {
-        setTotalToPay((subTotal).toLocaleString('es-CL'))
+        setTotalToPay(subTotal)
     }, [cart])
+
+    const increaseQuantity = (productId) => {
+        const updatedCart = cart.map((product) => 
+            product.id === productId
+            ? {...product, quantity: (product.quantity || 1) + 1 }
+            : product
+        )
+        setCart(updatedCart)
+        toast.success(`Agregado al Carrito!`, 
+        {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
+    }
+
+    const decreaseQuantity = (productId) => {
+        const updatedCart = cart.map((product) => 
+            product.id === productId
+            ? {...product, quantity: (product.quantity || 1) - 1 } // Disminuir la cantidad en 1
+            : product
+       ).filter((product) => product.quantity > 0) // Filtrar las pizzas con cantidad mayor que 0
+       setCart(updatedCart)
+       toast.error(`Eliminado del Carrito!`, 
+        {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
+    }
 
     return (
         <>
@@ -35,7 +75,7 @@ const Cart = () => {
                             <tr className="border-bottom">
                                 <th scope="col" className="py-3">PRODUCTO</th>
                                 {/* Se muestra solo en md y pantallas más grandes */}
-                                <th scope="col" className="py-3 d-none d-md-table-cell">CANTIDAD</th>
+                                <th scope="col" className="py-3">CANTIDAD</th>
                                 <th scope="col" className="py-3">TOTAL</th>
                             </tr>
                         </thead>
@@ -47,14 +87,18 @@ const Cart = () => {
                                         {product.name}
                                     </td>
                                     {/* Se muestra solo en md y pantallas más grandes */}
-                                    <td className="py-5 d-none d-md-table-cell">{product.quantity}</td>
-                                    <td className="py-5">${product.price}</td>
+                                    <td className="py-5">
+                                        <Button onClick={() => decreaseQuantity(product.id)} className='bg-danger mx-2'>-</Button>
+                                        {product.quantity}
+                                        <Button onClick={() => increaseQuantity(product.id)} className='bg-secondary mx-2'>+</Button>
+                                    </td>
+                                    <td className="py-5">${(product.price).toLocaleString('es-CL')}</td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
                     <div>
-                        <h4 className="text-md-end text-center mt-5">Subtotal: ${subTotal}</h4>
+                        <h4 className="text-md-end text-center mt-5">Subtotal: ${(subTotal).toLocaleString('es-CL')}</h4>
                         <p className="text-md-end text-center">Solo faltan los gastos de envío</p>
                         <div className="d-flex justify-content-end">
                             <Button className='col-lg-4 col-12 btn py-3 rounded-pill btn-secondary text-white fw-bold shadow-lg'>
