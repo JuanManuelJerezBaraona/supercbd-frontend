f0f4c6b364791c8f896363817a8492c55edf216d
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-02-13 10:32:52 -0300

    Refactor Cart component and update product names

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -68,31 +68,30 @@ const Cart = () => {
     return (
         <>
             <section className="container-fluid bg-primary text-white border-top">
-                <div className='row w-75 mx-auto pb-5'>
+                <div className='row col-12 col-md-8 mx-auto pb-5'>
                     <h2 className='display-5 py-5'>Tu Carrito</h2>
                     <table>
                         <thead>
                             <tr className="border-bottom">
-                                <th scope="col" className="py-3">PRODUCTO</th>
-                                {/* Se muestra solo en md y pantallas más grandes */}
-                                <th scope="col" className="py-3">CANTIDAD</th>
-                                <th scope="col" className="py-3">TOTAL</th>
+                                <th scope="col" className="py-3">Producto</th>
+                                <th scope="col" className="py-3">Cantidad</th>
+                                <th scope="col" className="py-3">Total</th>
                             </tr>
                         </thead>
                         <tbody>
                             {cart.map((product, index) => (
                                 <tr key={index} className="border-bottom">
-                                    <td className="py-5">
-                                        <img src={product.img} alt={product.name} className="me-2" style={{width: "50px", height: "auto"}}/>
-                                        {product.name}
+                                    <td className="py-4 col-5">
+                                        <img src={product.img} alt={product.name} className="rounded-circle shadow-lg mb-3" width="100"/>
+                                        <p>{product.name}</p>
+                                        <p className='m-0'>${(product.price).toLocaleString('es-CL')}</p>
                                     </td>
-                                    {/* Se muestra solo en md y pantallas más grandes */}
-                                    <td className="py-5">
-                                        <Button onClick={() => decreaseQuantity(product.id)} className='bg-danger mx-2'>-</Button>
+                                    <td className='py-4 col-5'>
+                                        <Button onClick={() => decreaseQuantity(product.id)} className='bg-danger py-1 rounded-circle mx-2'>-</Button>
                                         {product.quantity}
-                                        <Button onClick={() => increaseQuantity(product.id)} className='bg-secondary mx-2'>+</Button>
+                                        <Button onClick={() => increaseQuantity(product.id)} className='bg-secondary py-1 rounded-circle mx-2'>+</Button>
                                     </td>
-                                    <td className="py-5">${(product.price).toLocaleString('es-CL')}</td>
+                                    <td className="py-4 col-2">${(product.price * (product.quantity || 1)).toLocaleString('es-CL')}</td>
                                 </tr>
                             ))}
                         </tbody>
