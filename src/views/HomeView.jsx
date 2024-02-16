393e1a3aa429f92b9a683d0036540024e5075cd7
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-02-16 12:44:17 -0300

    Update HomeView component with additional content

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -9,10 +9,10 @@ const HomeView = () => {
         <>
             <Hero />
 
-            <section className="container-fluid bg-primary text-white">
+            <section className="container-fluid bg-primary text-white border-bottom">
                 <div className="row w-75 mx-auto py-5">
-                    <h2 className="display-5">Sobre Nosotros</h2>
                     <div className="col-12">
+                        <h2 className="display-5">Sobre Nosotros</h2>
                         <p className="m-0" style={{lineHeight: '2'}}>¡Hola! Soy Juan Jerez. Fui inspirado por mi propia experiencia al observar los beneficios del CBD en mi vida. Esta inspiración me llevó a fundar SUPER CBD con el objetivo de compartir los beneficios del CBD con el mundo.</p>
                     </div>
 
@@ -33,6 +33,25 @@ const HomeView = () => {
                             }}
                             className="col-12 col-md-6 col-lg-3 btn py-3 rounded-pill btn-secondary text-white fw-bold shadow-lg mt-4"
                         >Ver Nosotros</NavLink>
+                    </div>
+                </div>
+            </section>
+
+            <section className="container-fluid bg-primary text-white">
+                <div className="row w-75 mx-auto py-5">
+                    <div className="col-12 col-md-6">
+                        <h2 className="display-5">¿Qué es el CBD?</h2>
+                        <p className="m-0" style={{lineHeight: '2'}}>El Cannabidiol (CBD) es uno de los cannabinoides medicinales que se encuentran en las plantas de cáñamo. El CBD a diferencia del THC, no es adictivo, psicotrópico, ni psicoactivo, o sea que "no te vuela". El CBD es el cannabinoide más estudiado por la ciencia, debido a sus grandes beneficios para la salud mental y física.</p>
+                        <NavLink 
+                            to="/nosotros" 
+                            onClick={() => {
+                                window.scrollTo({top: 0, behavior: 'instant'});
+                            }}
+                            className="col-12 col-lg-6 btn py-3 rounded-pill btn-secondary text-white fw-bold shadow-lg mt-4"
+                        >Ver Preguntas</NavLink>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        
                     </div>
                 </div>
             </section>
