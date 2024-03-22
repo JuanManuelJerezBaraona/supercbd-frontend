f3417a1d6a18e24d9a19e82374c2e8f3da7fe7a4
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-03-22 11:50:21 -0300

    Actualiza el título del carrito a "Tu Carritoooo"

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -87,7 +87,7 @@ const Cart = () => {
         <>
             <section className="container-fluid bg-primary text-white border-top">
                 <div className='row col-12 col-md-8 mx-auto pb-5'>
-                    <h2 className='display-5 py-5'>Tu Carrito</h2>
+                    <h2 className='display-5 py-5'>Tu Carritoooo</h2>
                     <table>
                         <thead>
                             <tr className="border-bottom">
